48413bf5d925b46c7b40c5077af7df6410d44cc0
Ceaser Hernandez <45456228+CeaserH@users.noreply.github.com>
2019-11-21 13:24:11 -0800

    Revert "RestfulTasks"
    This reverts commit 3b963d2a68504ce9abf17281759c6c1fcadddeb6.

--- FetchData/RestfulTasks/server/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const TaskSchema = new mongoose.Schema({
-    title: {type: String},
-    description: { type: String, default: ""},
-    completed: { type: Boolean, default: false },
-}, { timestamps: true, collection: 'tasks'});
-
-mongoose.model("Task", TaskSchema);
